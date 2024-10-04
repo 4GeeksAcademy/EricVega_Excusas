0948da354abb184256d03953fc4aa5bd10e60750
EricVega <eric.veghe@gmail.com>
2024-10-04 16:56:51 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import ".style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Arrays para generar la excusa
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Función para obtener un valor aleatorio de un array
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  // Generar excusa concatenando las partes aleatorias
+  let excuse = `${getRandomElement(who)} ${getRandomElement(
+    action
+  )} ${getRandomElement(what)} ${getRandomElement(when)}.`;
+
+  // Asignar la excusa generada al innerHTML del párrafo con id "excuse"
+  document.getElementById("excuse").innerHTML = excuse;
 };
